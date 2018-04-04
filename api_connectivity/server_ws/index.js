b2e4c25379428ce9f45b79ab9d8805176236cd92
David Rodriguez <david.rmarco@babel.es>
2018-04-04 13:57:18 +0200

    WebSockets

---
+++ api_connectivity/server_ws/index.js
@@ -0,0 +1,16 @@
+var WebSocketServer = require( 'ws' ).Server;
+var wss = new WebSocketServer({port: 8085});
+wss.on('connection', function(ws) {
+
+    ws.on('message', (message) => {
+        console.log(' received: %s ', message) ;
+        setTimeout(() => {
+            console.log(' resending: %s ', message) 
+            ws.send(`Hola ${message}`)
+        }, 500);
+    });
+    /* ws.on ('message', function(message) {
+        console.log(' received: %s ', message) ;
+    }); */
+    ws.send('something') ;
+});
